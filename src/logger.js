3121b3f1978bda197c4834d09571eda14d412de6
Imen <imen.ammar@satoripop.com>
2018-03-21 09:14:24 +0100

    Add tests to all color

--- src/logger.js
+++ src/logger.js
@@ -30,7 +30,12 @@ const supportsColor = require('supports-color');
 if (supportsColor.stdout) {
 	console.log('Terminal stdout supports color');
 }
-
+if (supportsColor.stdout.has256) {
+	console.log('Terminal stdout supports 256 colors');
+}
+if (supportsColor.stderr.has16m) {
+	console.log('Terminal stderr supports 16 million colors (truecolor)');
+}
 let instance = null;
 class Logger {
   constructor() {
